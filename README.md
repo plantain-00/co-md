86079d947c09ae27d509999494d88c768616bf11
york yao <yaoao12306@outlook.com>
2016-07-23 07:03:19 +0800

    tsd to @types

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ The source code of a markdown online editor with cooperation.
 
 ## develop
 
-+ `npm run restore`
++ `npm i`
 + `tsc`
 + `npm run tslint`
 + `node md.js`
--- md.ts
+++ md.ts
@@ -1,27 +1,24 @@
-/// <reference path="typings/tsd.d.ts" />
-
 import * as express from "express";
 import * as passport from "passport";
-const session = require("express-session");
+import * as session from "express-session";
 const GitHubStrategy = require("passport-github").Strategy;
-import * as moment from "moment";
 import * as socketIO from "socket.io";
 
 import * as settings from "./settings";
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
     done(null, obj);
 });
 
 const strategy = new GitHubStrategy({
     clientID: settings.githubClientID,
-    clientSecret: settings.githubClientSecret
-}, (accessToken, refreshToken, profile, done) => {
-    process.nextTick(function() {
+    clientSecret: settings.githubClientSecret,
+}, (accessToken: string, refreshToken: string, profile: string, done: Function) => {
+    process.nextTick(function () {
         return done(null, profile);
     });
 });
@@ -33,7 +30,7 @@ const app = express();
 const sessionMiddleware = session({
     secret: settings.sessionSecret,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
 });
 
 app.use(sessionMiddleware);
@@ -44,33 +41,27 @@ app.use(express.static(__dirname + "/static"));
 
 app.get("/auth/github",
     passport.authenticate("github", { scope: [] }),
-    function(req, res) {
+    function (req, res) {
+        // todo
     });
 
 app.get("/auth/github/callback",
     passport.authenticate("github", { failureRedirect: "/" }),
-    function(req, res) {
+    function (req, res) {
         res.redirect("/");
     });
 
-app.get("/logout", function(req, res) {
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
 });
 
-app.get("/user", function(req, res) {
+app.get("/user", function (req, res) {
     res.json(req.user);
 });
 
 const server = app.listen(settings.port);
 console.log(`listening ${settings.port}`);
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
 
 const io = socketIO(server);
 const textNamespace = io.of("/text");
@@ -88,21 +79,26 @@ function leaveAll(socketId: string) {
     });
 }
 
-textNamespace.use(function(socket, next) {
-    sessionMiddleware(socket.request, {}, next);
+type Text = {
+    text: string;
+    room: string;
+}
+
+textNamespace.use(function (socket, next) {
+    sessionMiddleware(socket.request, {} as any, next);
 }).on("connection", socket => {
     if (socket.request.session.passport === undefined) {
         socket.disconnect(true);
     } else {
         const user = socket.request.session.passport.user;
-        socket.on("text changed", data => {
+        socket.on("text changed", (data: Text) => {
             textNamespace.to(data.room)
                 .emit("text changed", {
                     userId: user.id,
-                    text: data.text
+                    text: data.text,
                 });
         });
-        socket.on("enter", text => {
+        socket.on("enter", (text: string) => {
             socket.leaveAll();
             leaveAll(socket.id);
 
@@ -111,11 +107,11 @@ textNamespace.use(function(socket, next) {
                 socketIds.set(text, [socket.id]);
             } else {
                 const sockets = socketIds.get(text);
-                if (sockets.findIndex(s => s === socket.id) === -1) {
+                if (sockets && sockets.findIndex(s => s === socket.id) === -1) {
                     sockets.push(socket.id);
                 }
             }
-            textNamespace.to(text).emit("people count changed", socketIds.get(text).length);
+            textNamespace.to(text).emit("people count changed", socketIds!.get(text) !.length);
         });
         socket.on("disconnect", () => {
             leaveAll(socket.id);
